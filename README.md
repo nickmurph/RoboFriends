60ba88bab565db8658949863dd4ff73e670b544f
nickmurph <nfmurphy@buffalo.edu>
2022-12-19 21:43:30 -0700

    initial commit
    This project was built while working through the ZtM Web Dev course and was initially a sub-folder in the relevant repo (https://github.com/nickmurph/ZeroToMastery-Web-Dev). Extracted and copied here for the sake of visibility and any future modifications/extensions

---
+++ README.md
@@ -0,0 +1,7 @@
+# RoboFriends
+    A small users directory project, written during the ZeroToMastery Web Dev course to familiarize with React, Components, Create React App, JSX, and REST API integration.
+
+# Running the RoboFriends project
+    1. Clone the repo
+    2. Run npm install
+    3. Run npm start
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import './App.css'
+import Scroll from '../components/Scroll'
+
+
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchField: '',
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}) //replace {robots: users} with {} to test the empty robots loading page
+        )
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    }
+
+    render(){
+        const {robots, searchField} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        if (!robots.length){ //robots.length being zero evaluates to false, any length > 0 evalutes to true. Invert it to match the logic
+            return <h1 className='tc f1'>Loading</h1>
+        }else {
+        return (
+            <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <Scroll>
+                <CardList robots={filteredRobots}/>
+            </Scroll>
+            </div>
+        );
+        }
+    }
+}
+
+export default App;
